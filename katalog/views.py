8155a8d0f0126c5d9ed04d9a89fe4e2dd2daae94
itsjamessw <james.smith@ui.ac.id>
2022-09-12 12:58:32 +0700

    finishing views.py routing and mapping data

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    data_barang_katalog = CatalogItem.objects.all()
+    context = {
+        'list_barang': data_barang_katalog,
+        'nama': 'James Smith Wigglesworth',
+        'NPM': '2106750225',
+    }
+
+    return render(request, "katalog.html", context)
+
+
